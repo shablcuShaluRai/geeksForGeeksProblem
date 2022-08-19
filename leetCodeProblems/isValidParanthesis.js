5702bf01c54154ab12ec8fd1c0ff54d646b1615c
Shalu Kumari <16433381+shablcuShaluRai@users.noreply.github.com>
2022-08-19 20:17:34 +0530

    Is Valid Paranthesis solution using two pointer

---
+++ leetCodeProblems/isValidParanthesis.js
@@ -0,0 +1,23 @@
+function isValid(str) {
+  let start = 0,
+    last = str.length - 1;
+  const bracketObj = {
+    "{": "}",
+    "[": "]",
+    "(": ")"
+  };
+  // start = 0, 1
+  //last = 7, 6
+  while (start <= last) {
+    // "}" === "}"
+    if (bracketObj[str[start]] === str[last]) {
+      last--; // 6 ,5, 4, 3
+      start++; //1, 2, 3, 4
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log("isValid", isValid("{[()]}"));
