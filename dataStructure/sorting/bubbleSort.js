a1bca2f22cbbcb6f656ebb7ff3243676ff8f1cd2
shablcuShaluRai <shaluraics22@gmail.com>
2023-06-28 17:14:42 +0530

    implement bubble sort

--- dataStructure/sorting/bubbleSort.js
+++ dataStructure/sorting/bubbleSort.js
@@ -1,20 +1,36 @@
-function bubbleSort(arr) {
-  if (arr.length === 1) return arr;
-  function swap(i, j) {
-    let n;
-    n = arr[j];
-    arr[j] = arr[i];
-    arr[i] = n;
-  }
+// function bubbleSort(arr) {
+//   if (arr.length === 1) return arr;
+//   function swap(i, j) {
+//     let n;
+//     n = arr[j];
+//     arr[j] = arr[i];
+//     arr[i] = n;
+//   }
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        swap(i, j);
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i; j < arr.length; j++) {
+//       if (arr[i] > arr[j]) {
+//         swap(i, j);
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// another way to write the bubble sort
+function bubbleSort(arr) {
+  let temp;
+  for (let i = 0; i < arr.length; ++i) {
+    for (let j = 0; j < arr.length - 1 - i; ++j) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }
   return arr;
 }
+
 module.exports = bubbleSort;
-console.log(bubbleSort([6]));
+console.log(bubbleSort([98, 6, 54, 12, 4, 89]));
