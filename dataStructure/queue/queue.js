c2418ec0eefaf1ba3441bca92086d0f4aafe740f
shablcuShaluRai <shaluraics22@gmail.com>
2023-07-02 13:36:40 +0530

    Implemented queue using js

---
+++ dataStructure/queue/queue.js
@@ -0,0 +1,66 @@
+// Implement queue using js
+
+class Queue {
+  constructor() {
+    (this.items = {}), (this.count = 0);
+    this.lowestCount = 0;
+  }
+
+  // Enqueue: It add new element back of th queue or it adds element in the beginning of the array
+  // and returns length of the array
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count++;
+    return this.count;
+  }
+  // Dequeue: This methods removes first element from queue, it also return removed element
+
+  dequeue() {
+    if (this.isEmpty()) return undefined;
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return result;
+  }
+
+  // peek or front: this method returns first element in the queue
+
+  peek() {
+    if (this.isEmpty()) return undefined;
+    return this.items[this.lowestCount];
+  }
+
+  // it returns true if there is no element in the queue otherwise returns false
+
+  isEmpty() {
+    return this.count - this.lowestCount === 0;
+  }
+
+  //size or length: returns number of elements in the queue
+
+  size() {
+    if (this.isEmpty()) return [];
+    return this.count - this.lowestCount;
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+}
+
+const queue = new Queue();
+console.log("queue is empty", queue.isEmpty());
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(60);
+queue.clear();
+queue.enqueue(80);
+queue.enqueue(50);
+console.log("dequeue", queue.dequeue());
+
+console.log("queue", queue);
+console.log("queue is empty", queue.isEmpty());
+console.log("queue", queue.peek());
+console.log("queue", queue.size());
