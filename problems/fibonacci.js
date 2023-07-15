b3209b8d301494ab8fb36d6657079ea53da5d21a
shablcuShaluRai <shaluraics22@gmail.com>
2023-07-15 15:26:40 +0530

    implemented fibonacci series using memo

---
+++ problems/fibonacci.js
@@ -0,0 +1,29 @@
+// function fibonacci(n) {
+//   const memo = [1, 1];
+//   const memoFibonacci = (n) => {
+//     console.log("memo", memo);
+//     if (memo[n] !== undefined) {
+//       return memo[n];
+//     }
+//     memo[n] = memoFibonacci(n - 1) + memoFibonacci(n - 2);
+//     return memo[n];
+//   };
+//   return memoFibonacci(n);
+// }
+
+// console.log("fibonacci", fibonacci(10));
+
+function fibonacci(n) {
+  const memo = { 0: 1, 1: 1 };
+  const memoFibonacci = (n) => {
+    console.log("memo", memo);
+    if (memo[n] !== undefined) {
+      return memo[n];
+    }
+    memo[n] = memoFibonacci(n - 1) + memoFibonacci(n - 2);
+    return memo[n];
+  };
+  return memoFibonacci(n);
+}
+
+console.log("fibonacci", fibonacci(10));
